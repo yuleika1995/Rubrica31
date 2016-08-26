79b7494039c0d9d21e6cb1eb84c46160905ab5db
yuleika1995 <yuleika1995@yuleika1995>
2016-08-26 11:47:32 -0500

    2 Commit

--- RepositorioIngenieriaII/src/repositorioingenieriaii/RepositorioIngenieriaII.java
+++ RepositorioIngenieriaII/src/repositorioingenieriaii/RepositorioIngenieriaII.java
@@ -16,6 +16,8 @@ public class RepositorioIngenieriaII {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Bienvenido a java");
     }
     
 }
